a0eb780193ec26b519ed4e3fcaedf5c0e6207431
BDsnake <295935489@qq.com>
2025-01-09 09:08:36 +0800

    '6'

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import ComicView from "@/views/ComicView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
